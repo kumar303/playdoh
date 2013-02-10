fc7bd508548ff97f179ce93624947dcd23bf6dfc
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-02-09 17:30:45 -0800

    A starting point to run Playdoh on Stackato w/ pip

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -1,6 +1,10 @@
+import os
 from .base import *
 try:
     from .local import *
 except ImportError, exc:
-    exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
-    raise exc
+    if not STACKATO:
+        # Assume it's a local install or a non-stackato install.
+        # A local.py file may not be necessary for stackato VMs.
+        exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
+        raise exc
--- project/settings/base.py
+++ project/settings/base.py
@@ -1,6 +1,8 @@
 # This is your project's main settings file that can be committed to your
 # repo. If you need to override a setting locally, use settings_local.py
 
+import json
+import os
 from funfactory.settings_base import *
 
 # Name of the top-level module where you put all your apps.
@@ -11,6 +13,37 @@ PROJECT_MODULE = 'project'
 
 # Defines the views served for root URLs.
 ROOT_URLCONF = '%s.urls' % PROJECT_MODULE
+
+try:
+    import dj_database_url
+except ImportError:
+    dj_database_url = None
+
+if dj_database_url and os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config()
+        # 'slave': {
+        #     ...
+        # },
+    }
+    if 'mysql' in DATABASES['default']['ENGINE']:
+        opt = DATABASES['default'].get('OPTIONS', {})
+        opt['init_command'] = 'SET storage_engine=InnoDB'
+        opt['charset'] = 'utf8'
+        opt['use_unicode'] = True
+        DATABASES['default']['OPTIONS'] = opt
+    DATABASES['default']['TEST_CHARSET'] = 'utf8'
+    DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
+
+
+if os.environ.get('MEMCACHE_URL'):
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': os.environ['MEMCACHE_URL'],
+            'TIMEOUT': 500
+        }
+    }
 
 INSTALLED_APPS = list(INSTALLED_APPS) + [
     # Application base, containing global templates.
@@ -36,7 +69,17 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-SITE_URL = 'http://127.0.0.1:8000'
+if os.environ.get('VCAP_APPLICATION'):
+    VCAP_APP = json.loads(os.environ['VCAP_APPLICATION'])
+else:
+    VCAP_APP = None
+
+# True if we running as a Stackato instance.
+STACKATO = bool(VCAP_APP)
+
+# TODO(Kumar): check for https?
+SITE_URL = ('http://%s' % VCAP_APP['uris'][0] if VCAP_APP
+            else 'http://127.0.0.1:8000')
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = 'examples.home'
 LOGIN_REDIRECT_URL_FAILURE = 'examples.home'
@@ -78,3 +121,23 @@ DOMAIN_METHODS['messages'] = [
 # ]
 
 LOGGING = dict(loggers=dict(playdoh = {'level': logging.DEBUG}))
+
+if os.environ.get('DJANGO_SECRET_KEY'):
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+else:
+    # This must be set in settings/local.py
+    SECRET_KEY = ''
+
+if os.environ.get('DJANGO_HMAC_KEY'):
+    HMAC_KEYS = {
+        'stackato': os.environ['DJANGO_HMAC_KEY']
+    }
+    from django_sha2 import get_password_hashers
+    PASSWORD_HASHERS = get_password_hashers(BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+if STACKATO:
+    # Currently, Mozilla's Stackato only has a self-signed https cert.
+    # TODO: fix this when we have https support.
+    SESSION_COOKIE_SECURE = False
+    # TODO: remove this when we have a way to see exceptions on Stackato.
+    DEBUG = TEMPLATE_DEBUG = True
